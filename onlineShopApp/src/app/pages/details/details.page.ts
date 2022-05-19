1961b4bd6849a598ff6d0127e529c021d2fa9831
Raducu Vlad-Rares <vlad101vlad@gmail.com>
2022-05-19 16:24:30 +0300

    Added admin buttons for editing existing products

--- onlineShopApp/src/app/pages/details/details.page.ts
+++ onlineShopApp/src/app/pages/details/details.page.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import productData from '../../../assets/company/menu.json';
 import categoryData from '../../../assets/company/categories.json';
 import { CartService } from '../../services/cart.service';
+import {AuthenticationService} from '../../services/authentication.service';
 
 @Component({
   selector: 'app-details',
@@ -12,13 +13,26 @@ import { CartService } from '../../services/cart.service';
 export class DetailsPage implements OnInit {
   product = null;
   category = null;
+  isAdminLoggedIn: boolean;
+
 
   constructor(
     private route: ActivatedRoute,
-    private cartService: CartService
+    private cartService: CartService,
+    private authenticationService: AuthenticationService
   ) { }
 
   ngOnInit() {
+    this.loadDummyData();
+    this.subscribeToLoginStatus();
+  }
+
+  subscribeToLoginStatus() {
+    this.authenticationService.getLoginStatus()
+      .subscribe(authenticationStatus => this.isAdminLoggedIn = authenticationStatus);
+  }
+
+  loadDummyData() {
     const id = this.route.snapshot.paramMap.get('id');
 
     this.product = productData.filter((p) => p.id === id)[0];
@@ -29,5 +43,17 @@ export class DetailsPage implements OnInit {
 
   addToCart() {
     this.cartService.addProduct(this.product);
+  }
+
+  edit() {
+
+  }
+
+  delete() {
+
+  }
+
+  editImage() {
+
   }
 }
